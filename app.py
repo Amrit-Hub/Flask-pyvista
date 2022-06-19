21dd72e141b1a515c4ffd64f0876236e04fb2419
Amrit-Hub <itsamrit4u@gmail.com>
2022-06-19 20:11:03 +0530

    added javascript to update src without reload

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ template_path = os.path.join('templates')
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
     return render_template('index.html')
+
 
 @app.route('/submit', methods=['POST'])
 def submit():
@@ -34,6 +36,7 @@ def submit():
         plotter.screenshot(filepath)
         return render_template('index.html', img_src=filepath)
 
+
 @app.route('/submit3d', methods=['POST'])
 def submit3d():
     if request.method == "POST":
@@ -58,6 +61,31 @@ def submit3d():
         pl.background_color = 'white'
         pl.export_html(filepath)
         return render_template('index.html', img_src=filepath)
+
+
+@app.route('/<meshtype>', methods=['GET', 'POST'])
+def getimage(meshtype):
+    if meshtype == 'Sphere':
+        mesh = pyvista.Sphere()
+    elif meshtype == 'Cube':
+        mesh = pyvista.Cube()
+    elif meshtype == 'Bohemian Dome':
+        mesh = pyvista.ParametricBohemianDome()
+    elif meshtype == 'Cylinder':
+        mesh = pyvista.Cylinder()
+    else:
+        # invalid entry
+        raise ValueError('Invalid Option')
+    filename = meshtype + '.html'
+    filepath = os.path.join(static_image_path, filename)
+
+    # create a plotter and add the mesh to it
+    pl = pyvista.Plotter(window_size=(600, 600))
+    pl.add_mesh(mesh)
+    pl.background_color = 'white'
+    pl.export_html(filepath)
+    return filepath
+
 
 if __name__ == '__main__':
     app.run(debug=True)
